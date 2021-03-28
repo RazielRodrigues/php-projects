1f092b2f02049e1217e109a6d7ed15485157839f
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-03-28 04:38:38 -0300

    variaveis variaveis

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+
+$a = 'Nossa';
+$Nossa = 'Eu';
+$Eu = 'consegui';
+$consegui = 'respoonder';
+$respoonder = 'esse';
+$esse = 'desafio';
+
+//echo "nossa eu cconsegui responder esse desafio!";
+// $a
+
+echo "<br>";
+echo "{$a}";
+echo "<br>";
+echo "{$$a}";
+echo "<br>";
+echo "{$$$a}";
+echo "<br>";
+echo "{$$$$a}";
+echo "<br>";
+echo "{$$$$$a}";
+echo "<br>";
+echo "{$$$$$$a}";
