d6311d404ad1cd9e387964a9074fe73b63fd4c4e
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-03-28 05:10:27 -0300

    ponteiros

--- index.php
+++ index.php
@@ -25,3 +25,34 @@ echo "<br>";
 echo "{$$$$$a}";
 echo "<br>";
 echo "{$$$$$$a}";
+
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+echo "PONTEIROS:";
+echo "<br>";
+
+$variavel = 'valor inicial...variavel';
+var_dump($variavel);
+
+//atribuição por valor
+$variavelValor = $variavel;
+var_dump($variavelValor);
+$variavelValor = 'agora recebeu outro valor...';
+var_dump($variavel);
+var_dump($variavelValor);
+
+//atribuição por ponteiro
+
+// 1. A variavel $ponteiro tem o poder de substituir o que se encontra em 
+// $variavel pois foi atribuida o &
+$ponteiro = &$variavel;
+var_dump($ponteiro);
+
+// 2. dessa forma se o valor da variavel $ponteiro for alterado, o valor da $variavel também será.
+$ponteiro = 'mesma referencia';
+var_dump($variavel);
+
+// echo "$variavel $ponteiro";
