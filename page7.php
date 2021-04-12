dcd7559a7af1925b76c0d44c1cd7d50c3fb39148
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-04-11 22:14:43 -0300

    __get

--- page7.php
+++ page7.php
@@ -1,28 +1,54 @@
 <h1>Metodos magicos</h1>
+var_dump(
+
+);
+/*
+    ? ...
+*/
 <?php
 class Pessoa{
     public $nome;
     public $idade;
 
-    function __construct($nome,$idade){
+    public function __construct($nome,$idade){
         echo 'invocado <BR>';
         $this->nome = $nome;
         $this->idade = $idade;
     }
 
-    function __destruct(){
+    public function __destruct(){
         echo 'liberado <BR>';
     }
 
+    /*
+        ? Esse metodo transforma o contexto do objeto em string de forma que,
+        ? seja possivel renderizar o objeto apontando para $this
+        ? Transforma os atributos em string
+    */
     public function __toString(){
        return "{$this->nome} tem {$this->idade} anos";
     }
 
+    //! Acessando objeto apenas com o this
     public function apresentar(){
         echo $this . "<BR>";
+    }
+
+    /*
+        ? ...
+    */
+    public function __get($atr){
+        echo "Não declarado: {$atr}";
     }
     
 }
 
 $pessoa = new Pessoa('raziel', 22);
-$pessoa->apresentar();
+//? teste toString: $pessoa->apresentar();
+
+$pessoa->nomeCompleto;
+var_dump(
+
+
+
+);
