266b31f4d8a8338e20e48fc741a8700e19746678
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-04-11 23:14:00 -0300

    polimorsfimso pt 2

--- page8.php
+++ page8.php
@@ -2,9 +2,15 @@
 <?php
 
 //! conceito mais presente em linguagens tipadas
+//! No php se passa o tipo pela função.
 
+//? Classe abstrata pois será apenas usada de superclasse
 abstract class Comida {
- public $peso;
+    public $peso;
+}
+
+abstract class Bebida{
+    public $peso;
 }
 
 class Arroz extends Comida{}
@@ -15,6 +21,8 @@ class Feijão extends Comida{}
 
 class Sorvete extends Comida{}
 
+class Suco extends Bebida{};
+
 class Pessoa {
 
     public $peso;
@@ -23,6 +31,16 @@ class Pessoa {
         $this->peso = $peso;
     }
 
+
+    /*
+        ? O segredo está em qual parametro voce vai passar para a função
+        ? como eu passei Comida qualquer classe que tenha herança de Comida
+        ? será aceita pela função.
+
+        ? Ou seja serve para amarrar os tipos qur vão entrar na função
+        ? Mas o PHP já é uma linguagens que trabalha assim naturalmente.
+
+    */
     public function comer(Comida $comida){
         $this->peso += $comida->peso;
     }
@@ -38,9 +56,14 @@ $janta->peso = 0.10;
 $ceia = new Sorvete();
 $ceia->peso = 1.50;
 
+$suco = new Suco();
+$suco->peso = 0.20;
+
 $raziel = new Pessoa(65.00);
 $raziel->comer($almoco);
 $raziel->comer($janta);
 $raziel->comer($ceia);
 
+
+//! gera erro $raziel->comer($suco);
 echo $raziel->peso;
